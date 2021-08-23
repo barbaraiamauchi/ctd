1f5d740ab05cadda7063b05e5be5d73cd07a19b7
barbaraiamauchi <79321344+barbaraiamauchi@users.noreply.github.com>
2021-08-23 20:54:55 -0300

    Upload materiais materias Programação imperativa

---
+++ BIMESTRE-1/Progamação-imperativa/aula05/ex1/script.js
@@ -0,0 +1,12 @@
+
+// declaração (criar a função)
+function saudar(saudacao) {
+    alert(saudacao);
+}
+
+function cumprimentar() {
+    alert('olá');
+}
+
+//invocação (chamar)
+saudar('cansado?'); //Método
