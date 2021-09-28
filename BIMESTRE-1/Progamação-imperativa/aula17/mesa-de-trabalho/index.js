56b284b879555e6a057804f3f5a8a91e967aa4e8
Barbara <barbaraiamauchi@gmail.com>
2021-09-28 11:28:59 -0300

    aula 27/09

---
+++ BIMESTRE-1/Progamação-imperativa/aula17/mesa-de-trabalho/index.js
@@ -0,0 +1,32 @@
+/*
+    Atividade I: Inicialize um projeto npm;
+    (  ) Preencha nome do projeto;
+    (  ) Preencha o nome de todos os integrantes do grupo;
+    (  ) Preencha a licença como UNLICENSED;
+    (  ) Preencha a versão como 1.0.
+
+    Atividade II: Instale o Nodemon globalmente em sua máquina. Link com a instrução de instalação: https://www.npmjs.com/package/nodemon.
+    
+    Atividade III: Ignore a pasta node_modules
+    (  ) Configure o git, utilizando o arquivo .gitignore, para que ele ignore as alterações realizas na pasta node_modules.
+
+    Atividade IV: Crie o projeto 
+    (  ) Instale o Express no projeto (https://expressjs.com/pt-br/),
+    (  ) Implemente a aplicação como as seguintes rotas:
+        (  ) Rota http://localhost:1234/
+            (  ) Insira o texto: Seja bem-vindo ao [nome_do_projeto].
+        (  ) Rota http://localhost:1234/sobre/
+            (  ) Insire o texto: Sobre o projeto
+
+
+    Comandos npm
+        npm init -> para inicializar o projeto.
+        npm install [nome_do_modulo] -> para instalar um módulo de terceiros.
+        npm install -> para instalar o projeto quando esta sem a pasta node_modules.
+        npm uninstall [nome_do_modulo] -> para desinstalar um módulo de terceiros.
+        npm start -> para executar o projeto. 
+*/
+
+*/**/node_modules
+
+console.log('oi teste');
